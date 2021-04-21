adf4868c784e6b44e80fb3ec5f1f7c0f0fa7e4ec
ivandespinosa@gmail.com <ivandespinosa@gmail.com>
2021-04-21 15:08:11 -0500

    Seccion 12 - Google

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ app.use( express.json() );
 // Base de datos
 dbConnection();
 
+// Directorio público
+app.use( express.static('public') );
+
 // Username: mean_user
 // Password: nVzoDMZtFzenXKE6
 
