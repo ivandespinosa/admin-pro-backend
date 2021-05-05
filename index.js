f45d7ff19eecc828a6a13fda5ee735f7d6c8e478
ivandespinosa@gmail.com <ivandespinosa@gmail.com>
2021-05-05 18:12:12 -0500

    SPA control

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 const cors = require('cors');
 const express = require('express');
@@ -30,6 +31,11 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/upload', require('./routes/uploads'));
+
+// Lo último
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
+});
 
 // app.get( '/', (req, res) => {
 //     res.json({
